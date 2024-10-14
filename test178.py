8dc4906d1077a584b5b81d8933069d158b567312
RupeshVeluru <roope28@gmail.com>
2024-10-14 15:58:48 +0530

    Test

---
+++ test178.py
@@ -0,0 +1,25 @@
+# Use case of string case conversion mentiods
+
+str1="abc"
+str2=str1.upper()
+print(str1,str2)
+
+str3="ABC"
+str4=str1.lower()
+print(str3,str4)
+
+str5="python programming language"
+str6=str5.title()
+print(str5,str6)
+
+str7="PYTHON PROGRAMMING"
+str8=str7.title()
+print(str7,str8)
+
+str9="python"
+str10=str9.capitalize()
+print(str9,str10)
+
+str11="PythoN"
+str12=str11.swapcase()
+print(str11,str12)
